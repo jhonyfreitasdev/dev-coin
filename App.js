33f30f818dbe9906b9826d9915929dbf2199efd8
Jhony Freitas <jhony00._@hotmail.com>
2024-04-30 22:49:21 -0300

    Criando app e requisições do projeto

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import CurrentPrice from './src/components/CurrentPrice';
+import {
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  Platform
+} from 'react-native';
+import HisttoryGraphics from './src/components/HistoryGraphic';
+import QuotationsList from './src/components/QuotationsList';
 
 export default function App() {
+  const [coinsDataList, setCoinsDataList] = useState([]);
+  const [days, setDays] = useState(7);
+
+  useEffect(() => {
+    async function fetchCoinsData() {
+      const coinsData = await getCoinsDataGraphic(days);
+      setCoinsDataList(coinsData);
+    }
+    fetchCoinsData();
+  }, [days]);
+
+  const createUrl = (daysQuantity) => {
+    const date = new Date();
+    const endDateString = date.toISOString().split('T')[0];
+    const endDate = endDateString;
+
+    date.setDate(date.getDate() - daysQuantity)
+    const startDateString = date.toISOString().split('T')[0];
+    const startDate = startDateString;
+
+    return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+  };
+
+  const getCoinsDataGraphic = async (daysQuantity) => {
+    const response = await fetch(createUrl(daysQuantity));
+    const json = await response.json();
+    const bpiList = await json.bpi;
+    return Object.entries(bpiList).map((item) => (
+      { date: item[0], value: item[1] }
+    ));
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor="#a1810c"
+        barStyle="light-content"
+      />
+      <CurrentPrice coinsDataList={coinsDataList}/>
+      <HisttoryGraphics coinsDataList={coinsDataList}/>
+      <QuotationsList setDays={setDays} coinsDataList={coinsDataList} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 40 : 0
   },
 });
